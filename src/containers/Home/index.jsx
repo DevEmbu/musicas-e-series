a2161120dbc13e36f18a4ffeff6d19b095270888
DevEmbu <jideoliveira72@gmail.com>
2023-11-15 12:35:57 -0300

    SEGUINDO EM FRENTA

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,20 +1,24 @@
 {/*INDEX HOME do container */}
 
+import { useState } from "react"
 import api from "../../services/api"
 import { Background } from "./styles"
 
 function Home(){
+  const [movie, setMovie] = useState()
+
  async function getMovies(){
     const data = await api.get('/movie/popular')  
-    console.log(data)  
+    
+    setMovie(data.data.results[1])
   }
   getMovies()
     return(
-        <Background img="https://image.tmdb.org/t/p/original/kuf6dutpsT0vSVehic3EZIqkOBt.jpg">            
-            <h1>Home</h1>
+        <Background img="https://api.themoviedb.org/3/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg">            
+            <h1>{movie.title}</h1>
             <p>Esta é uma pagina Home</p>
             <h2>MODULO II REACT</h2>
-            <h3>Estilizando o Background</h3>
+            <h3>useStates - REACT II</h3>
         </Background>
     )
 }
